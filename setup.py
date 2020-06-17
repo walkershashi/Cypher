7b7c4e8a1c1e47ceb16c5dc44ddbec78a100e727
shashiwalker <skssunny30@gmail.com>
2020-06-17 08:31:03 +0530

    added a setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name = 'cypher',
+    version = '0.1',
+    author = 'shashi',
+    author_email = 'skssunny30@gmail.com',
+    description = 'Password Encryptor by suggesting wheather a password is strong or not',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/walkershashi/Cypher',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ] 
+)
