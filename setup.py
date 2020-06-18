b641ffd66f05d1d2d7f03463cba59f1e98e67706
shashiwalker <skssunny30@gmail.com>
2020-06-18 15:55:01 +0530

    Removed long_description

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setuptools.setup(
     author = 'shashi',
     author_email = 'skssunny30@gmail.com',
     description = 'Password Encryptor by suggesting wheather a password is strong or not',
-    long_description = long_description,
     long_description_content_type = 'text/markdown',
     url = 'https://github.com/walkershashi/Cypher',
     packages = setuptools.find_packages(),
