6953fb3c63bf72224a08e15012964cdd4f5fe2de
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2020-09-30 23:16:32 +0530

    Updates to setup.py and README.md

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+# def readme():
+#     with open('README.md') as f:
+#         retun f.read()
+
+
+setup(
     name = 'cypher',
     version = '1.0',
     author = 'shashi',
     author_email = 'skssunny30@gmail.com',
     description = 'Password Encryptor by suggesting wheather a password is strong or not',
+    #long_description = readme(),
     long_description_content_type = 'text/markdown',
-    url = 'https://github.com/walkershashi/Cypher',
-    packages = setuptools.find_packages(),
-    classifiers = [
+    url = "https://github.com/walkershashi/Cypher",
+     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ] 
-)
+    ], 
+    lisence = 'MIT',
+    packages = ['cypher'],
+    )
